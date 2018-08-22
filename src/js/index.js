e9e864a8f777dfa9ee8cb8c3d79556480ee20f30
alex.paclin <alex.paclin@gmail.com>
2018-08-22 23:55:03 +0500

    8. Planning our Project Architecture with MVC

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module! Variable x is ${x}`);
+import str from './models/Search';
+//import { add as a, multiply as m, ID } from './views/searchView';
+import * as searchView from './views/searchView';
+
+//console.log(`Using imported functions! add: ${a(ID, 2)} multiply: ${m(3, 5)} str: ${str}`);
+console.log(`Using imported functions! add: ${searchView.add(searchView.ID, 2)} multiply: ${searchView.multiply(3, 5)} str: ${str}`);
