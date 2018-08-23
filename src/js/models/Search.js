d9685667120fd3bf24538824fea83f5d2965d555
alex.paclin <alex.paclin@gmail.com>
2018-08-23 15:03:09 +0500

    13. Building the Search View - Part 1

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -10,8 +10,7 @@ export default class Search {
         const key = 'a9be2a5d628b7c4eb90d208f9a14f4e7';
         try {
             const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
-            this.result = res.data.recipes;
-            //console.log(this.result);
+            this.result = res.data.recipes;            
         } catch (error) {
             alert(error);
         }
