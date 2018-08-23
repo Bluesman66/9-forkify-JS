288fa9caafd471ef9b2fc79819ffcc569dfe0a89
alex.paclin <alex.paclin@gmail.com>
2018-08-23 23:02:37 +0500

    16. Implementing Search Results Pagination

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,30 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements, renderLoader, clearLoader } from './views/base';
+import {
+    elements,
+    renderLoader,
+    clearLoader
+} from './views/base';
 
 /** Global state of the app 
-* - Search object
-* - Current recipe object
-* - Shopping list object
-* - Liked recipes   
-*/
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes   
+ */
 const state = {};
 
 const controlSearch = async () => {
     // 1. Get query from the view
-    const query = searchView.getInput();    
+    const query = searchView.getInput();
 
     if (query) {
         // 2. New search object and add to state
         state.search = new Search(query);
-        
+
         // 3. Prepare UI for results
         searchView.clearInput();
-        searchView.clearResult();
+        searchView.clearResults();
         renderLoader(elements.searchRes);
 
         // 4. Search for recipes
@@ -37,3 +41,11 @@ elements.searchForm.addEventListener('submit', e => {
     controlSearch();
 });
 
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -6,7 +6,7 @@ export default class Search {
     }
 
     async getResults() {
-        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const proxy ='';//'https://cors-anywhere.herokuapp.com/';
         const key = 'a9be2a5d628b7c4eb90d208f9a14f4e7';
         try {
             const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
