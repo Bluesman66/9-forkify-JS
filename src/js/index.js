a2fecaa48dfaf331963c7fd95c04cc1a2c31b0b9
alex.paclin <alex.paclin@gmail.com>
2018-08-23 12:23:45 +0500

    11. Building the Search Model

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = 'a9be2a5d628b7c4eb90d208f9a14f4e7';
-    try {
-        const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (error) {
-        alert(error);
-    }
-}
-
-getResults('pizza');
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,20 @@
-export default 'I am an export string.';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {        
+        this.query = query;        
+    }
+
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = 'a9be2a5d628b7c4eb90d208f9a14f4e7';
+        try {
+            const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
+
