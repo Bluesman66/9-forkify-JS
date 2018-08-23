92e0f7a3f32fec52f2022c2f56d7fa87136168d6
alex.paclin <alex.paclin@gmail.com>
2018-08-23 11:47:16 +0500

    10. Making our First API Calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,15 @@
-import str from './models/Search';
-//import { add as a, multiply as m, ID } from './views/searchView';
-import * as searchView from './views/searchView';
+import axios from 'axios';
 
-//console.log(`Using imported functions! add: ${a(ID, 2)} multiply: ${m(3, 5)} str: ${str}`);
-console.log(`Using imported functions! add: ${searchView.add(searchView.ID, 2)} multiply: ${searchView.multiply(3, 5)} str: ${str}`);
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = 'a9be2a5d628b7c4eb90d208f9a14f4e7';
+    try {
+        const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+}
+
+getResults('pizza');
